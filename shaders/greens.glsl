4de1b46e417d435e8ba83b73c482693a1add6a81
Thomas Bailey <baileytb7@gmail.com>
2025-12-20 16:41:39 -0500

    quicksave

--- shaders/greens.glsl
+++ shaders/greens.glsl
@@ -10,7 +10,7 @@ layout(location = 0) uniform int gridSize;
 layout(location = 1) uniform int nGrids;
 layout(location = 2) uniform float G;
 layout(location = 3) uniform float worldSize;
-layout(location = 4) uniform float epsilon = 1e-9;
+layout(location = 4) uniform float epsilon = 1e-7;
 
 layout(rg32f, binding = 0) readonly uniform image3D inputTexture;
 layout(rg32f, binding = 1) writeonly uniform image3D outGradX;
@@ -23,49 +23,47 @@ const float PI = 3.14159265359;
 const float TWO_PI = 2.0 * PI;
 
 float getWaveNumber(int i, int N, float L) {
-    int halfN = N / 2;
-    int shifted = (i <= halfN) ? i : (i - N);
-    return float(shifted) * (TWO_PI / L);
+  int halfN = N / 2;
+  int shifted = (i <= halfN) ? i : (i - N);
+  return float(shifted) * (TWO_PI / L);
 }
 
-vec2 mult_ik(vec2 c, float k) {
-    return vec2(-c.y * k, c.x * k);
-}
+vec2 mult_ik(vec2 c, float k) { return vec2(-c.y * k, c.x * k); }
 
 void main() {
-    ivec3 pos = ivec3(gl_GlobalInvocationID);
-    ivec3 texSize = imageSize(inputTexture);
+  ivec3 pos = ivec3(gl_GlobalInvocationID);
+  ivec3 texSize = imageSize(inputTexture);
 
-    if (any(greaterThanEqual(pos, texSize)))
-        return;
+  if (any(greaterThanEqual(pos, texSize)))
+    return;
 
-    float kx = getWaveNumber(pos.x, gridSize, worldSize);
-    float ky = getWaveNumber(pos.y, gridSize, worldSize);
+  float kx = getWaveNumber(pos.x, gridSize, worldSize);
+  float ky = getWaveNumber(pos.y, gridSize, worldSize);
 
 #if DIM == 2
-    float k2 = kx * kx + ky * ky;
-    float geoFactor = -2.0 * PI;
+  float k2 = kx * kx + ky * ky;
+  float geoFactor = -2.0 * PI;
 #else
-    float kz = getWaveNumber(pos.z, gridSize, worldSize);
-    float k2 = kx * kx + ky * ky + kz * kz;
-    float geoFactor = -4.0 * PI;
+  float kz = getWaveNumber(pos.z, gridSize, worldSize);
+  float k2 = kx * kx + ky * ky + kz * kz;
+  float geoFactor = -4.0 * PI;
 #endif
 
-    vec2 rhoHat = imageLoad(inputTexture, pos).xy;
-    vec2 phiHat = vec2(0.0);
+  vec2 rhoHat = imageLoad(inputTexture, pos).xy;
+  vec2 phiHat = vec2(0.0);
 
-    if (k2 > epsilon) {
-        phiHat = rhoHat * (geoFactor * G / k2);
-    }
+  if (k2 > epsilon) {
+    phiHat = rhoHat * (geoFactor * G / k2);
+  }
 
-    vec2 gradXHat = mult_ik(phiHat, kx);
-    vec2 gradYHat = mult_ik(phiHat, ky);
+  vec2 gradXHat = mult_ik(phiHat, kx);
+  vec2 gradYHat = mult_ik(phiHat, ky);
 
-    imageStore(outGradX, pos, vec4(gradXHat, 0.0, 0.0));
-    imageStore(outGradY, pos, vec4(gradYHat, 0.0, 0.0));
+  imageStore(outGradX, pos, vec4(gradXHat, 0.0, 0.0));
+  imageStore(outGradY, pos, vec4(gradYHat, 0.0, 0.0));
 
 #if DIM == 3
-    vec2 gradZHat = mult_ik(phiHat, kz);
-    imageStore(outGradZ, pos, vec4(gradZHat, 0.0, 0.0));
+  vec2 gradZHat = mult_ik(phiHat, kz);
+  imageStore(outGradZ, pos, vec4(gradZHat, 0.0, 0.0));
 #endif
 }
