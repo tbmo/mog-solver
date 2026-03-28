c521dae76febf813dca362439c988f1c7b60ecaa
tbmo <28987369+tbmo@users.noreply.github.com>
2026-03-27 21:44:31 -0400

    windowsdev2

--- shaders/greens.glsl
+++ shaders/greens.glsl
@@ -10,8 +10,8 @@ layout(location = 0) uniform int gridSize;
 layout(location = 1) uniform int nGrids;
 layout(location = 2) uniform float G;
 layout(location = 3) uniform float worldSize;
-// layout(location = 4) uniform float epsilon = 5e-4;
 layout(location = 4) uniform float epsilon = 1e-1;
+// layout(location = 4) uniform float epsilon = 1e-1;
 
 layout(rg32f, binding = 0) readonly uniform image3D inputTexture;
 layout(rg32f, binding = 1) writeonly uniform image3D outGradX;
@@ -51,13 +51,14 @@ void main() {
   vec2 rhoHat = imageLoad(inputTexture, pos).xy;
   vec2 phiHat = vec2(0.0);
 
-  if (k2 > epsilon) {
-    phiHat = rhoHat * (geoFactor * G / k2);
-  }
+  // if (k2 > epsilon) {
+  //   phiHat = rhoHat * (geoFactor * G / k2);
+  // }
+  float cellSize = worldSize / float(gridSize);
+  float a = cellSize * 0.5; // tune this: 0.5–2.0 cells
+  float k2_soft = k2 + 1.0 / (a * a);
+  phiHat = rhoHat * (geoFactor * G / k2_soft);
 
-  // Use a soft rolloff:
-  // float k2_reg = k2 + epsilon; // Simple regularization
-  // phiHat = rhoHat * (geoFactor * G / k2_reg);
   vec2 gradXHat = mult_ik(phiHat, kx);
   vec2 gradYHat = mult_ik(phiHat, ky);
 
